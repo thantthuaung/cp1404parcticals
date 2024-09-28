d3443a59cd79d1147fa82059ca69b4ecc76a8801
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-09-28 23:44:19 +0800

    add new functions to score file

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,23 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
+def main():
+    score = int(input("Enter score: "))
+    result = determine_result(score)
+    print(result)
 
-score = float(input("Enter score: "))
+    random_number = random.randint(0,100)
+    result_for_random_number = determine_result(random_number)
+    print(result_for_random_number)
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score< 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+def determine_result(score):
+    if score < 0 or score > 100:
+        return"Invalid score"
+    elif score < 50:
+        return"Bad"
+    elif score < 90:
+        return"Passable"
+    else:
+        return"Excellent"
+main()
