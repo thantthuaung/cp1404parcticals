5178ccea564030a0c083f6e5961f82c7ba55489c
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-10-12 16:02:49 +0800

    new

--- prac_01/hello.py
+++ prac_01/hello.py
@@ -1,1 +1,7 @@
-print("hello world")
+number_of_special = 0
+password = "t2000"
+SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
+for character in password:
+    if character in SPECIAL_CHARACTERS:
+        number_of_special += 1
+print(len(password))
