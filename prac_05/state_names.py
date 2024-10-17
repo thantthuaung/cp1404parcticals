350fcc89c16cc4f1f2a0edda6161de762dc92423
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-10-17 15:39:22 +0800

    add EAFP approach and output line up

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,14 +5,14 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD":"Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
-
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+short_name_width = max(len(key) for key in CODE_TO_NAME.keys())
+for key, value in CODE_TO_NAME.items():
+    print(f"{key:{short_name_width}} is {value}")
+while True:
+    try:
+        state_code = input("Enter short state: ").upper()
+        print(f"{state_code:{short_name_width}} is {CODE_TO_NAME[state_code]}")
+    except KeyError:
+        print(f"Invalid Input")
