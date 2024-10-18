c32301835625a627546fd1e931062534da30a593
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-10-18 16:12:28 +0800

    add word occurrences file without sorting the output

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,16 @@
+"""
+Word Occurrences
+Estimate: 20 minutes
+Actual:   24 minutes
+"""
+
+text_counts = {}
+user_input = input("Text: ")
+list_of_texts = user_input.split()
+for text in list_of_texts:
+    if text not in text_counts.values():
+        text_counts[text] = 1
+    else:
+        text_counts[text] += 1
+    max_width = max(len(text) for text in list_of_texts)
+    print(f"{text:{max_width}}:{text_counts[text]}")
