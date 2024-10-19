0fd7144d059883acd4c5a43ce93e77190b36b1d3
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-10-20 00:47:05 +0800

    refactor the whole thing and add function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,12 +4,21 @@ PLAYER_POSITION = 2
 
 
 def main():
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-        data = []
-        for line in in_file:
-            row = line.strip().split(",")
-            data.append(row)
+    data = get_data_from_csv()
 
+    champion_amount, things = count_of_champions(data)
+
+    display_output(champion_amount, things)
+
+
+def display_output(champion_amount, things):
+    for key, value in champion_amount.items():
+        print(f"{key}:{value}")
+    print(f"\nThese {len(list(things))} countries have won Wimbledon: ")
+    print(", ".join(sorted(list(things))))
+
+
+def count_of_champions(data):
     champion_amount = {}
     things = set()
     for part in data[NUMBER_ONE:]:
@@ -18,11 +27,14 @@ def main():
             champion_amount[part[PLAYER_POSITION]] = NUMBER_ONE
         else:
             champion_amount[part[PLAYER_POSITION]] += NUMBER_ONE
-
-    for key, value in champion_amount.items():
-        print(f"{key}:{value}")
-    print(f"\nThese {len(list(things))} countries have won Wimbledon: ")
-    print(", ".join(list(things)))
+    return champion_amount, things
 
 
+def get_data_from_csv():
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        data = []
+        for line in in_file:
+            row = line.strip().split(",")
+            data.append(row)
+    return data
 main()
