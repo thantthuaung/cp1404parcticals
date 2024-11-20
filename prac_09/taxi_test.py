32c8e4e35c33f91794f65f20e88665630ea5867e
thantthuaung <thantthuaung@my.jcu.edu.au>
2024-11-21 01:21:10 +0800

    add test Taxi files

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,11 @@
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+distance = my_taxi.drive(40)
+price = my_taxi.get_fare()
+print(f"{my_taxi} and ${price}")
+
+my_taxi.start_fare()
+distance_2 = my_taxi.drive(100)
+price_2 = my_taxi.get_fare()
+print(f"{my_taxi} and ${price_2}")
